d3378b3b4ab672bcf04c14d8c676a8b40c4d0522
kinjal gajjar <kinjalgajjar1942@gmail.com>
2022-03-31 11:43:48 +0530

    main page

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tictac/fonts/Press_Start_2P/splashscreen.dart';
 
-import 'homepage.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
